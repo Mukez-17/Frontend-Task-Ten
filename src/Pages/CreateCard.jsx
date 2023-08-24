d1d57c2ae55c372354bd5c1e33cbb47ebfd343d9
Mukesh Dev <124699989+Mukez-17@users.noreply.github.com>
2023-08-24 14:44:43 +0530

    Update CreateCard.jsx

--- src/Pages/CreateCard.jsx
+++ src/Pages/CreateCard.jsx
@@ -1,23 +1,27 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { CardGroup } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function CreateCard({titleCard, cardItem}) {
   return (
     <>
         <h4 className='place-h4'>Must Visit Place in {titleCard.Heading}</h4>
-        <CardGroup className="body">
-            {cardItem.map((item) => (
-                <Card className="card-item" key={item.id}>
-                    <Card.Img variant="top" style={{borderRadius:"8px 8px 0 0"}} src={item.img} alt={item.placeName} height="200"/>
-                    <Card.Body>
-                        <Card.Title><h5>{item.placeName}</h5></Card.Title>
-                        <Card.Text style={{textAlign:"justify"}}>{item.description}</Card.Text>
-                        <Button variant="primary" className='justify-content-start'>Read More</Button>
-                    </Card.Body>
-                </Card>
-            ))}
-        </CardGroup>
+        <Container className='card-container'>
+            <Row className='row-card'>
+                {cardItem.map((item) => (
+                    <Col key={item.id} sm={12} md={6} lg={4} xl={3} className='card-col'>
+                        <Card className="card-item">
+                            <Card.Img variant="top" style={{borderRadius:"8px 8px 0 0"}} src={item.img} alt={item.placeName} height="200"/>
+                            <Card.Body>
+                                <Card.Title><h5>{item.placeName}</h5></Card.Title>
+                                <Card.Text style={{textAlign:"justify"}}>{item.description}</Card.Text>
+                                <Button variant="primary" className='justify-content-start'>Read More</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     </>
   );
 }
